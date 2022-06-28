4f4fa7b240185e6d88b59ce67d2ed9f0182cacd1
Angelica <67800009+alstar555@users.noreply.github.com>
2022-06-28 15:01:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function login(){
+	var email = document.getElementById("email").value
+	var pass = document.getElementById("password").value
+	window.alert("you've been hacked, you password is " + pass);
+	//window.location.replace("https://www.instagram.com/accounts/login/");
+}
